f9d0799963081d6b8683aa6f830a91a474c3bdcc
Santiago Gimeno <santiago.gimeno@quantion.es>
2015-03-16 12:35:23 +0100

    ChangeGC fix?

--- examples/simple/creategc.js
+++ examples/simple/creategc.js
@@ -8,8 +8,9 @@ x11.createClient(function(err, display) {
     var black = display.screen[0].black_pixel;
 
     var wid = X.AllocID();
-    X.CreateWindow(wid, root, 0, 0, 400, 300, 0, 0, 0, 0, { backgroundPixel: white, eventMask: PointerMotion }); 
+    X.CreateWindow(wid, root, 0, 0, 400, 300, 0, 0, 0, 0, { backgroundPixel: white, eventMask: PointerMotion });
     var gc = X.AllocID();
-    X.CreateGC(gc, wid);     
+    X.CreateGC(gc, wid);
+    X.ChangeGC(gc, { lineStyle : 1 });
     X.MapWindow(wid);
 });
